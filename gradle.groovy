93114961a331c917146c86bbd913ffc01b7415f4
SpawnCL <rpineda.ilabaca@gmail.com>
2022-11-21 11:29:31 -0300

    Fixinig Jenkinsfile

---
+++ gradle.groovy
@@ -0,0 +1,13 @@
+def gradle_compile()
+        {
+                sh './mvnw clean compile -e'
+        }
+def mavel_test()
+        {
+                sh './mvnw clean test -e'
+        }
+def mavel_package()
+        {
+                sh './mvnw clean package -e'
+        }
+
