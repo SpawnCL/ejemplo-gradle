b0725dde1c3f76f9e4c3f03cd038392b7e72b0c7
SpawnCL <rpineda.ilabaca@gmail.com>
2022-11-21 13:53:04 -0300

    Fixing gradle.groovy with gradle clean build

--- gradle.groovy
+++ gradle.groovy
@@ -1,9 +1,8 @@
 def gradle_build_test_jar()
         {
                 sh "gradle build"
-
+                sh "gradle clean build" 
         }
-
 def gradle_run()
         {
                 sh "gradle bootRun &"
